bac77512db47bdca5147cef6129538433a41b5db
Fernando Mendes <fernando.mendes@xpi.com.br>
2019-12-09 16:57:37 -0300

    novo

---
+++ FilaPedido/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FilaPedido
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            var pedidos = new[]{
+                  new {Pedido = "VVFFF", Item = "Batata", Quantidade = 2},
+                  new {Pedido = "VVTTF", Item = "Batata", Quantidade = 1},
+                  new {Pedido = "VVTTF", Item = "Batata", Quantidade = 1},
+                  new {Pedido = "VVTTF", Item = "Batata", Quantidade = 1},
+                  new {Pedido = "VVTTF", Item = "Batata", Quantidade = 1},
+                  new {Pedido = "VVTTF", Item = "Batata", Quantidade = 1},
+                  new {Pedido = "VVTTF", Item = "Batata", Quantidade = 1},
+                  new {Pedido = "VVTTF", Item = "Batata", Quantidade = 1},
+                  new {Pedido = "VVTTF", Item = "Batata", Quantidade = 1},
+                  new {Pedido = "VVTTF", Item = "Batata", Quantidade = 1},
+                  new {Pedido = "VVTTF", Item = "Batata", Quantidade = 1},
+                  new {Pedido = "VVTTF", Item = "Batata", Quantidade = 1},
+                  new {Pedido = "VVTTF", Item = "Batata", Quantidade = 1},
+                  new {Pedido = "VVTTF", Item = "Batata", Quantidade = 1},
+                  new {Pedido = "VVTTF", Item = "Batata", Quantidade = 1},
+                  new {Pedido = "VVTTF", Item = "Batata", Quantidade = 1},
+                  new {Pedido = "VVTTF", Item = "Batata", Quantidade = 1},
+                  new {Pedido = "VVTTF", Item = "Batata", Quantidade = 1},
+                  new {Pedido = "VVTTF", Item = "Batata", Quantidade = 1},
+                  new {Pedido = "VVHHF", Item = "Batata", Quantidade = 3}
+              };
+
+            var itens = new[]{ "Batata", "Contra Filé"
+                  
+                  
+              };
+
+
+
+            dbView.ItemsSource = pedidos;
+            listItem.ItemsSource = itens;
+            
+        }
+
+        private void DbView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Senha win2 = new Senha();
+            win2.Show();
+        }
+    }
+}
---
+++ FilaPedido/Window1.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FilaPedido
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            var pedidos = new List<Item>();
+            
+
+            pedidos.Add(new Item() { Pedido = "092114", Descricao = "BATATA", Quantidade = 5, Tipo = "NOVO" });
+            pedidos.Add(new Item() { Pedido = "092114", Descricao = "BATATA", Quantidade = 5, Tipo = "NOVO" });
+            pedidos.Add(new Item() { Pedido = "092114", Descricao = "BATATA", Quantidade = 5, Tipo = "NOVO" });
+            pedidos.Add(new Item() { Pedido = "092114", Descricao = "BATATA", Quantidade = 5, Tipo = "EM PREPARAÇÃO" });
+
+
+            dgItem.ItemsSource = pedidos;
+            this.Contar();
+        }
+
+        private void DgItem_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            var item = e.Row.Item as Item;
+            if (item != null)
+            {
+                e.Row.FontSize = 18;
+                e.Row.FontWeight = FontWeights.Bold;
+
+                if (item.Tipo == "NOVO")
+                    e.Row.Foreground = new SolidColorBrush(Colors.Green);
+                else
+                    e.Row.Foreground = new SolidColorBrush(Colors.Orange);
+            }
+        }
+
+        private void Contar()
+        {
+            this.lblEntrada.FontSize = 60;
+            this.lblEntrada.Content = 19;
+            this.lblEntrada.FontWeight = FontWeights.Bold;
+            this.lblEntrada.Foreground = new SolidColorBrush(Colors.Green);
+            this.lblEntrada.HorizontalContentAlignment = HorizontalAlignment.Center;
+
+            this.lblPreparacao.FontSize = 60;
+            this.lblPreparacao.Content = 1;
+            this.lblPreparacao.FontWeight = FontWeights.Bold;
+            this.lblPreparacao.Foreground = new SolidColorBrush(Colors.Orange);
+            this.lblPreparacao.HorizontalContentAlignment = HorizontalAlignment.Center;
+
+            this.lblSaida.FontSize = 60;
+            this.lblSaida.Content = 100;
+            this.lblSaida.FontWeight = FontWeights.Bold;
+            this.lblSaida.Foreground = new SolidColorBrush(Colors.Blue);
+            this.lblSaida.HorizontalContentAlignment = HorizontalAlignment.Center;
+        }
+    }
+
+    public class Item
+    {
+        public string Pedido { get; set; }
+        public string Descricao { get; set; }
+        public int Quantidade { get; set; }
+        public string Tipo { get; set; }
+    }
+}
